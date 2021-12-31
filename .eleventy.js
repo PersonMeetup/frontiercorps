a972d831cbe95f0e45dfebed52b5f7e5d8d9ad1d
Leslie Swan <42787368+PersonMeetup@users.noreply.github.com>
2021-12-31 15:22:26 -0700

    restructure news page (#2)
    This change also gives proper styling to the tag pages, as they reuse
    the same code assets.
    Further styling can be done but what's currently here is ready to ship.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,24 @@
 const markdownIt = require("markdown-it");
 
+// Portions of code sourced from https://github.com/11ty/eleventy-base-blog
 module.exports = function(eleventyConfig) {
     eleventyConfig.setDataDeepMerge(true);
 
     // Parse the licence.md file
     eleventyConfig.addPairedShortcode("credits", function(content) {
         return markdownIt().render(content);
+    });
+
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if(!Array.isArray(array) || array.length === 0) {
+            return [];
+        }
+        if( n < 0 ) {
+            return array.slice(n);
+        }
+
+        return array.slice(0, n);
     });
 
     // Copy media folders to the output
